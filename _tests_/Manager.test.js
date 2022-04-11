2ac512391974942c773449e5525aa67f28825f20
joshuad1590 <joshuad15@gmail.com>
2022-04-10 22:55:59 -0400

    added lib items

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,12 +1,12 @@
 const { expect } = require('@jest/globals');
 const { test } = require('picomatch');
 const Manager = require('../lib/Manager');
-const manager = new Manager('');
+const manager = new Manager('john', '12', 'john@gmail.com', '555-5555');
 
 test('to see if the manager object constructors will work', () => {
-    expect(manager.name).toBe('');
-    expect(manager.id).toBe('');
-    expect(manager.email).toBe('');
-    expect(manager.number).toBe('');
+    expect(manager.name).toBe('john');
+    expect(manager.id).toBe('12');
+    expect(manager.email).toBe('john@gamil.com');
+    expect(manager.number).toBe('555-5555');
 });
 
